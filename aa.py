ead2e0094599ce93b14eb61398e769e9f937bc39
ammathew <ammathew.nyc@gmail.com>
2014-06-23 22:56:15 -0400

    scraped a pdf

--- aa.py
+++ aa.py
@@ -2,13 +2,16 @@ from BeautifulSoup import BeautifulSoup, SoupStrainer
 import requests
 from xgoogle.search import GoogleSearch, SearchError
 import pickle
-import simplejson
 import re
 import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from table_def import Letters
+import urllib2
+import scraperwiki
+import pdfminer
 
+pdf_url = 'http://www.sec.gov/divisions/investment/13f/13flist2013q4.pdf'
 
 searchTerms = [ 'shareholder letter', 'letter to shareholders' ]
 
@@ -55,9 +58,39 @@ def addToDb( texts ):
    
    
              #links = getUrls( 'shareholder letters' )
-f = open('output.txt', 'r')
-links = pickle.load(f)
-print links
-texts = checkWords( links )
-addToDb( texts )
+#f = open('output.txt', 'r')
+#links = pickle.load(f)
+#print links
+#texts = checkWords( links )
+#addToDb( texts )
 
+print "HELLO"
+path = '13flist2013q4.pdf'
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+    fp.close()
+    device.close()
+    str = retstr.getvalue()
+    retstr.close()
+    print str
+
+convert_pdf_to_txt(path)
