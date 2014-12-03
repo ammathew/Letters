fcd02d4f361db664b5d138217e909ec1bb4c254b
ammathew <annmary1985@gmail.com>
2014-12-02 21:11:30 -0700

    highlight, click, and search now works

--- app/static/app.js
+++ app/static/app.js
@@ -1,7 +1,6 @@
+
 aa = angular.module('myServiceModule', ['ui.bootstrap', 'nvd3ChartDirectives', 'ngRoute' ]);
 aa.config(['$interpolateProvider', '$routeProvider', '$locationProvider', function ($interpolateProvider, $routeProvider, $locationProvider ) {
-    $interpolateProvider.startSymbol('[[');
-    $interpolateProvider.endSymbol(']]');
 
     $routeProvider
 	.when('/register',{
@@ -21,7 +20,79 @@ aa.config(['$interpolateProvider', '$routeProvider', '$locationProvider', functi
     // was not able to get html5Mode to work. maybe look into History.js .. 
 }]);
 
-aa.controller('DashboardCtrl', ['$scope', 'searchTwitterFactory', '$http', '$location', '$rootScope', function ($scope, searchTwitterFactory, $http, $location, $rootScope ) {
+aa.controller('DashboardCtrl', ['$scope', 'searchTwitterFactory', '$http', '$location', '$rootScope', '$window',function ($scope, searchTwitterFactory, $http, $location, $rootScope, $window ) {
+
+    $scope.getTweetEnts = function ( tweetText ) {
+	$scope.getEntities( tweetText ).success( function( data ) {
+            $scope.keywords = data;  
+        });
+    } 
+
+    $scope.getEntities = function ( text ) {
+	data = { text: text } 
+        return $http({ 
+            method: 'POST',
+	    headers: {
+		"Content-Type": "application/json"
+	    },
+	   data: data,
+	    url:"/api/extractEnts",
+        })
+    }
+
+    $scope.searchTwitter = function ( searchTerm ) {
+	data = { search_term : searchTerm };
+        return $http({ 
+            method: 'GET',
+	    params: data,
+	    url:"/api/searchTwitter",
+        })
+    }
+
+    /*
+    $scope.getHighlightedText = function() {
+	$('html').mouseup(function (e){
+	    var text = "";
+	    if (window.getSelection) {
+		var blah = window.getSelection();
+		console.log( blah );
+		$scope.searchText = window.getSelection().toString();
+	    } else if (document.selection && document.selection.type != "Control") {
+		$scope.searchText = document.selection.createRange().text;
+	    }
+	});
+    }	
+*/
+
+    $scope.getHighlightedText = function() {
+	$('html').mouseup(function (e){
+	    var text = "";
+	    if (window.getSelection && window.getSelection().type == "Range" ) {
+		$("mark").contents().unwrap();
+		$("mark").remove();
+		$scope.selectedText = window.getSelection().toString();
+		var selection = window.getSelection();
+		var range = selection.getRangeAt(0);
+		var cssclass = $(selection.anchorNode.parentNode).attr("class");
+		var newNode = document.createElement("mark");
+		range.surroundContents(newNode);
+
+	    } else if (document.selection && document.selection.type != "Control") {
+		$scope.searchText = document.selection.createRange().text;
+	    }
+	});
+    }	
+
+    $scope.$watch( 'selectedText', function( newValue ) {
+	$( "mark" ).click( function() {
+	    $scope.searchTwitter( newValue ).success( function( data ){
+		$scope.tweetsWithSearchTerm = data.statuses;
+	    }); 
+	})
+	
+    })
+   
+    $scope.getHighlightedText();
 
     $scope.logOut = function () {
           $http({ 
@@ -34,6 +105,22 @@ aa.controller('DashboardCtrl', ['$scope', 'searchTwitterFactory', '$http', '$loc
         });
     }
 
+    $scope.$watch( 'posts', function( newValue ) {
+	$scope.allPosts = '';
+
+	if( newValue ) {
+	    for( i=0; i< $scope.posts.length; i++ ) {
+		console.log( $scope.posts );
+		$scope.allPosts = $scope.allPosts + '. ' + $scope.posts[i].text;
+	    }
+	    console.log( 'get entities' );
+	    $scope.getEntities( $scope.allPosts ).success( function(data) {
+		$scope.entities = data;	
+	    });
+	}
+	console.log( $scope.allPosts );
+    })
+
     $scope.getPosts = function () {
           $http({ 
             method: 'GET',
@@ -41,6 +128,7 @@ aa.controller('DashboardCtrl', ['$scope', 'searchTwitterFactory', '$http', '$loc
         }).success( function( data ) {
             $scope.posts = data;
             console.log( data );  
+	    
         });
     }
 
@@ -49,58 +137,11 @@ aa.controller('DashboardCtrl', ['$scope', 'searchTwitterFactory', '$http', '$loc
             method: 'GET',
 	    url:"/api/authtwitter",
         }).success( function( data ) {
-            console.log( data );  
+            console.log( data.redirect_url );  
+            $window.location.href = data.redirect_url;
         });
     }
-
-    $scope.radius = 3;
-    //$scope.geoData = '';
-    $scope.searchTwitter = function( companyName, geoData ) {
-	console.log( searchTwitterFactory );
-	searchTwitterFactory( companyName, $rootScope.geoData ).success( function( data ){
-	    $scope.pieChartData = [];
-	    for( var i=0; i<2; i++ ){
-		$scope.pieChartData[i] = data[i]
-	    }
-	    $scope.posts = data[2];
-	    
-	});
-    };
-
-    $scope.$watchCollection( '[ address, radius ]' , function( newValues ) {
-	$scope.getGeocode( newValues[0] );	
-    });
-
-    $scope.getGeocode = function( address ) {	
-        //console.log( 'location' );
-        // was not able to get html5mode to work
-        //console.log( $location.path() );
-
-	var mapOptions = {
-	    address: address
-	}
-	geocoder = new google.maps.Geocoder();
-	geocoder.geocode( mapOptions, function( results ){ 
-	    $scope.geocode = results;
-	    $rootScope.geoData = $scope.geocode[0].geometry.location.k + "," + $scope.geocode[0].geometry.location.B+ "," + $scope.radius + "mi";
-	});
-	
-    };
-    $scope.geocode = {};
-
-    $scope.xFunction = function(){
-	return function(d) {
-            return d.key;
-	}
-    };
-
-    $scope.yFunction = function(){
-	return function(d) {
-	    console.log( d.y );
-            return d.y;
-	};
-    }
-
+  
 }]);
   
 aa.factory('searchTwitterFactory', function($http) {
@@ -194,6 +235,15 @@ aa.controller('AuthCtrl', ['$scope', '$http', '$location',  function ($scope, $h
 
 }])
 
+
+aa.directive('blah', function() {
+    return {
+	scope: '=',
+        restrict: 'E',  
+        templateUrl: "tweet.html"
+    };
+});
+
 aa.directive( 'reservation', function() {
     return {
 	restrict: 'A',
@@ -205,3 +255,11 @@ aa.directive( 'reservation', function() {
 	}	
     }
 })
+
+
+aa.run(function($rootScope, $templateCache) {
+   $rootScope.$on('$viewContentLoaded', function() {
+      $templateCache.removeAll();
+   });
+});
+
